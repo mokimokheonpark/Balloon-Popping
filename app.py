2be49093ceb78ae57e2e0e2aa029b7ac94a05d6c
Mokheon Park <mokimokheonpark@gmail.com>
2023-04-14 19:41:03 -0400

    modified app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import pygame
-##########################################################################################
-# Initialization
+
+# Pygame Initialized
 pygame.init()
 
 # Screen Size
@@ -14,9 +14,8 @@ pygame.display.set_caption("Balloon Popping")
 
 # FPS
 clock = pygame.time.Clock()
-##########################################################################################
 
-# 1. Background, Game Image, Coordinates, Speed, Font
+# Path
 current_path = os.path.dirname(__file__)
 image_path = os.path.join(current_path, "images")
 
@@ -47,54 +46,52 @@ weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
 weapon_size = weapon.get_rect().size
 weapon_width = weapon_size[0]
 
-# Weapons
-weapons = []
-
 # Weapon Movement Speed
 weapon_speed = 10
 
+# Weapon List Declared
+weapons = []
+
 # Balloon
-ball_images = [
+balloon_images = [
     pygame.image.load(os.path.join(image_path, "balloon1.png")),
     pygame.image.load(os.path.join(image_path, "balloon2.png")),
     pygame.image.load(os.path.join(image_path, "balloon3.png")),
     pygame.image.load(os.path.join(image_path, "balloon4.png")),]
 
 # Balloon Movement Speed
-ball_speed_y = [-18, -15, -12, -9]
+balloon_speed_y = [-18, -15, -12, -9]
 
-# Balloons
-balls = []
+# Balloon List Declared
+balloons = []
 
 # Initial Balloon
-balls.append({
+balloons.append({
     "pos_x" : 50,
     "pos_y" : 50,
     "img_idx" : 0,
     "to_x" : 3,
     "to_y" : -6,
-    "init_spd_y" : ball_speed_y[0]})
+    "init_spd_y" : balloon_speed_y[0]})
 
 # Weapon Removed, Balloon Removed
 weapon_to_remove = -1
-ball_to_remove = -1
+balloon_to_remove = -1
 
 # Font
 game_font = pygame.font.Font(None, 40)
-total_time = 100
+total_time = 30
 start_ticks = pygame.time.get_ticks()
 
-# Message
-# Game Over
-# Time Out
-# Mission Complete
+# Default Game Result
 game_result = "Game Over"
 
+# Main Loop
 running = True
 while running:
     dt = clock.tick(30)
 
-    # 2. Keyboard, Mouse
+    # Keyboard, Mouse
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -113,7 +110,7 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 character_to_x = 0
     
-    # 3. Character Position
+    # Character Position
     character_x_pos += character_to_x
 
     if character_x_pos < 0:
@@ -122,29 +119,24 @@ while running:
         character_x_pos = screen_width - character_width
     
     # Weapon Location
-    # ...
-    # ...
     weapons = [[w[0], w[1] - weapon_speed] for w in weapons]
 
     # Weapon Removed
     weapons = [[w[0], w[1]] for w in weapons if w[1] > 0]
 
     # Balloon Location
-    for ball_idx, ball_val in enumerate(balls):
+    for ball_idx, ball_val in enumerate(balloons):
         ball_pos_x = ball_val["pos_x"]
         ball_pos_y = ball_val["pos_y"]
         ball_img_idx = ball_val["img_idx"]
 
-        ball_size = ball_images[ball_img_idx].get_rect().size
+        ball_size = balloon_images[ball_img_idx].get_rect().size
         ball_width = ball_size[0]
         ball_height = ball_size[1]
 
-        # ...
         if ball_pos_x < 0 or ball_pos_x > screen_width - ball_width:
             ball_val["to_x"] *= -1
         
-        # ...
-        # ...
         if ball_pos_y >= screen_height - stage_height - ball_height:
             ball_val["to_y"] = ball_val["init_spd_y"]
         else:
@@ -153,20 +145,18 @@ while running:
         ball_val["pos_x"] += ball_val["to_x"]
         ball_val["pos_y"] += ball_val["to_y"]
 
-    # 4. Collision
-
-    # Character rect Information Update
+    # Character rect Information Updated
     character_rect = character.get_rect()
     character_rect.left = character_x_pos
     character_rect.top = character_y_pos
 
-    for ball_idx, ball_val in enumerate(balls):
+    for ball_idx, ball_val in enumerate(balloons):
         ball_pos_x = ball_val["pos_x"]
         ball_pos_y = ball_val["pos_y"]
         ball_img_idx = ball_val["img_idx"]
 
-        # Balloon rect Information Update
-        ball_rect = ball_images[ball_img_idx].get_rect()
+        # Balloon rect Information Updated
+        ball_rect = balloon_images[ball_img_idx].get_rect()
         ball_rect.left = ball_pos_x
         ball_rect.top = ball_pos_y
 
@@ -180,15 +170,15 @@ while running:
             weapon_pos_x = weapon_val[0]
             weapon_pos_y = weapon_val[1]
 
-            # Weapon rect Information Update
+            # Weapon rect Information Updated
             weapon_rect = weapon.get_rect()
             weapon_rect.left = weapon_pos_x
             weapon_rect.top = weapon_pos_y
 
-            # Collision Check
+            # Collision Checked
             if weapon_rect.colliderect(ball_rect):
                 weapon_to_remove = weapon_idx
-                ball_to_remove = ball_idx
+                balloon_to_remove = ball_idx
 
                 # Balloon Division
                 if ball_img_idx < 3:
@@ -197,27 +187,27 @@ while running:
                     ball_height = ball_rect.size[1]
 
                     # Divided Balloon Information
-                    small_ball_rect = ball_images[ball_img_idx + 1].get_rect()
+                    small_ball_rect = balloon_images[ball_img_idx + 1].get_rect()
                     small_ball_width = small_ball_rect.size[0]
                     small_ball_height = small_ball_rect.size[1]
 
                     # To Left Side
-                    balls.append({
+                    balloons.append({
                         "pos_x" : ball_pos_x + (ball_width / 2)- (small_ball_width / 2),
                         "pos_y" : ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
                         "img_idx" : ball_img_idx + 1,
                         "to_x" : -3,
                         "to_y" : -6,
-                        "init_spd_y" : ball_speed_y[ball_img_idx + 1]})
+                        "init_spd_y" : balloon_speed_y[ball_img_idx + 1]})
 
                     # To Right Side
-                    balls.append({
+                    balloons.append({
                         "pos_x" : ball_pos_x + (ball_width / 2)- (small_ball_width / 2),
                         "pos_y" : ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
                         "img_idx" : ball_img_idx + 1,
                         "to_x" : 3,
                         "to_y" : -6,
-                        "init_spd_y" : ball_speed_y[ball_img_idx + 1]})
+                        "init_spd_y" : balloon_speed_y[ball_img_idx + 1]})
 
                 break
         else:
@@ -225,30 +215,30 @@ while running:
         break
 
     # Weapon Collided, Balloon Collided
-    if ball_to_remove > -1:
-        del balls[ball_to_remove]
-        ball_to_remove = -1
+    if balloon_to_remove > -1:
+        del balloons[balloon_to_remove]
+        balloon_to_remove = -1
     
     if weapon_to_remove > -1:
         del weapons[weapon_to_remove]
         weapon_to_remove = -1
     
     # All Balloons Removed
-    if len(balls) == 0:
+    if len(balloons) == 0:
         game_result = "Mission Complete"
         running = False
 
-    # 5. screen.blit
+    # Screen.blit
     screen.blit(background, (0, 0))
 
     for weapon_x_pos, weapon_y_pos in weapons:
         screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
     
-    for idx, val in enumerate(balls):
+    for idx, val in enumerate(balloons):
         ball_pos_x = val["pos_x"]
         ball_pos_y = val["pos_y"]
         ball_img_idx = val["img_idx"]
-        screen.blit(ball_images[ball_img_idx], (ball_pos_x, ball_pos_y))
+        screen.blit(balloon_images[ball_img_idx], (ball_pos_x, ball_pos_y))
     
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
@@ -271,7 +261,8 @@ msg_rect = msg.get_rect(center = (int(screen_width / 2), int(screen_height / 2))
 screen.blit(msg, msg_rect)
 pygame.display.update()
 
-# Waiting for 2 seconds
+# Delay
 pygame.time.delay(2000)
 
+# Pygame Terminated
 pygame.quit()
