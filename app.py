3e9a217d57642c628d0a14bc1fdc6fe3e8ce6f07
Mokheon Park <mokimokheonpark@gmail.com>
2023-04-15 08:18:11 -0400

    modified app.py and README.md

--- app.py
+++ app.py
@@ -74,8 +74,10 @@ balloons.append({
     "to_y" : -6,
     "init_spd_y" : balloon_speed_y[0]})
 
-# Weapon Removed, Balloon Removed
+# Weapon Removed
 weapon_to_remove = -1
+
+# Balloon Removed
 balloon_to_remove = -1
 
 # Font
@@ -91,7 +93,7 @@ running = True
 while running:
     dt = clock.tick(30)
 
-    # Keyboard, Mouse
+    # Event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -125,43 +127,43 @@ while running:
     weapons = [[w[0], w[1]] for w in weapons if w[1] > 0]
 
     # Balloon Location
-    for ball_idx, ball_val in enumerate(balloons):
-        ball_pos_x = ball_val["pos_x"]
-        ball_pos_y = ball_val["pos_y"]
-        ball_img_idx = ball_val["img_idx"]
+    for balloon_idx, balloon_val in enumerate(balloons):
+        balloon_pos_x = balloon_val["pos_x"]
+        balloon_pos_y = balloon_val["pos_y"]
+        balloon_img_idx = balloon_val["img_idx"]
 
-        ball_size = balloon_images[ball_img_idx].get_rect().size
-        ball_width = ball_size[0]
-        ball_height = ball_size[1]
+        balloon_size = balloon_images[balloon_img_idx].get_rect().size
+        balloon_width = balloon_size[0]
+        balloon_height = balloon_size[1]
 
-        if ball_pos_x < 0 or ball_pos_x > screen_width - ball_width:
-            ball_val["to_x"] *= -1
+        if balloon_pos_x < 0 or balloon_pos_x > screen_width - balloon_width:
+            balloon_val["to_x"] *= -1
         
-        if ball_pos_y >= screen_height - stage_height - ball_height:
-            ball_val["to_y"] = ball_val["init_spd_y"]
+        if balloon_pos_y >= screen_height - stage_height - balloon_height:
+            balloon_val["to_y"] = balloon_val["init_spd_y"]
         else:
-            ball_val["to_y"] += 0.5
+            balloon_val["to_y"] += 0.5
         
-        ball_val["pos_x"] += ball_val["to_x"]
-        ball_val["pos_y"] += ball_val["to_y"]
+        balloon_val["pos_x"] += balloon_val["to_x"]
+        balloon_val["pos_y"] += balloon_val["to_y"]
 
     # Character rect Information Updated
     character_rect = character.get_rect()
     character_rect.left = character_x_pos
     character_rect.top = character_y_pos
 
-    for ball_idx, ball_val in enumerate(balloons):
-        ball_pos_x = ball_val["pos_x"]
-        ball_pos_y = ball_val["pos_y"]
-        ball_img_idx = ball_val["img_idx"]
+    for balloon_idx, balloon_val in enumerate(balloons):
+        balloon_pos_x = balloon_val["pos_x"]
+        balloon_pos_y = balloon_val["pos_y"]
+        balloon_img_idx = balloon_val["img_idx"]
 
         # Balloon rect Information Updated
-        ball_rect = balloon_images[ball_img_idx].get_rect()
-        ball_rect.left = ball_pos_x
-        ball_rect.top = ball_pos_y
+        balloon_rect = balloon_images[balloon_img_idx].get_rect()
+        balloon_rect.left = balloon_pos_x
+        balloon_rect.top = balloon_pos_y
 
         # Collision of Character and Balloon
-        if character_rect.colliderect(ball_rect):
+        if character_rect.colliderect(balloon_rect):
             running = False
             break
 
@@ -176,52 +178,53 @@ while running:
             weapon_rect.top = weapon_pos_y
 
             # Collision Checked
-            if weapon_rect.colliderect(ball_rect):
+            if weapon_rect.colliderect(balloon_rect):
                 weapon_to_remove = weapon_idx
-                balloon_to_remove = ball_idx
+                balloon_to_remove = balloon_idx
 
                 # Balloon Division
-                if ball_img_idx < 3:
+                if balloon_img_idx < 3:
                     # Current Balloon Size Information
-                    ball_width = ball_rect.size[0]
-                    ball_height = ball_rect.size[1]
+                    balloon_width = balloon_rect.size[0]
+                    balloon_height = balloon_rect.size[1]
 
                     # Divided Balloon Information
-                    small_ball_rect = balloon_images[ball_img_idx + 1].get_rect()
-                    small_ball_width = small_ball_rect.size[0]
-                    small_ball_height = small_ball_rect.size[1]
+                    small_balloon_rect = balloon_images[balloon_img_idx + 1].get_rect()
+                    small_ball_width = small_balloon_rect.size[0]
+                    small_ball_height = small_balloon_rect.size[1]
 
                     # To Left Side
                     balloons.append({
-                        "pos_x" : ball_pos_x + (ball_width / 2)- (small_ball_width / 2),
-                        "pos_y" : ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
-                        "img_idx" : ball_img_idx + 1,
+                        "pos_x" : balloon_pos_x + (balloon_width / 2)- (small_ball_width / 2),
+                        "pos_y" : balloon_pos_y + (balloon_height / 2) - (small_ball_height / 2),
+                        "img_idx" : balloon_img_idx + 1,
                         "to_x" : -3,
                         "to_y" : -6,
-                        "init_spd_y" : balloon_speed_y[ball_img_idx + 1]})
+                        "init_spd_y" : balloon_speed_y[balloon_img_idx + 1]})
 
                     # To Right Side
                     balloons.append({
-                        "pos_x" : ball_pos_x + (ball_width / 2)- (small_ball_width / 2),
-                        "pos_y" : ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
-                        "img_idx" : ball_img_idx + 1,
+                        "pos_x" : balloon_pos_x + (balloon_width / 2)- (small_ball_width / 2),
+                        "pos_y" : balloon_pos_y + (balloon_height / 2) - (small_ball_height / 2),
+                        "img_idx" : balloon_img_idx + 1,
                         "to_x" : 3,
                         "to_y" : -6,
-                        "init_spd_y" : balloon_speed_y[ball_img_idx + 1]})
+                        "init_spd_y" : balloon_speed_y[balloon_img_idx + 1]})
 
                 break
         else:
             continue
         break
 
-    # Weapon Collided, Balloon Collided
-    if balloon_to_remove > -1:
-        del balloons[balloon_to_remove]
-        balloon_to_remove = -1
-    
+    # Weapon Collided
     if weapon_to_remove > -1:
         del weapons[weapon_to_remove]
         weapon_to_remove = -1
+
+    # Balloon Collided
+    if balloon_to_remove > -1:
+        del balloons[balloon_to_remove]
+        balloon_to_remove = -1
     
     # All Balloons Removed
     if len(balloons) == 0:
@@ -235,10 +238,10 @@ while running:
         screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
     
     for idx, val in enumerate(balloons):
-        ball_pos_x = val["pos_x"]
-        ball_pos_y = val["pos_y"]
-        ball_img_idx = val["img_idx"]
-        screen.blit(balloon_images[ball_img_idx], (ball_pos_x, ball_pos_y))
+        balloon_pos_x = val["pos_x"]
+        balloon_pos_y = val["pos_y"]
+        balloon_img_idx = val["img_idx"]
+        screen.blit(balloon_images[balloon_img_idx], (balloon_pos_x, balloon_pos_y))
     
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
@@ -256,9 +259,9 @@ while running:
     pygame.display.update()
 
 # Message Printed
-msg = game_font.render(game_result, True, (255, 255, 0))
-msg_rect = msg.get_rect(center = (int(screen_width / 2), int(screen_height / 2)))
-screen.blit(msg, msg_rect)
+message = game_font.render(game_result, True, (255, 255, 0))
+message_rect = message.get_rect(center = (int(screen_width / 2), int(screen_height / 2)))
+screen.blit(message, message_rect)
 pygame.display.update()
 
 # Delay
